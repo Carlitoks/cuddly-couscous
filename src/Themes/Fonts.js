5909e6f30c0e04d5bdf6f22501c8c0237fa85041
Carlos Pereira <carlos00003@hotmail.com>
2018-03-14 20:54:39 -0400

    Adding new font

--- src/Themes/Fonts.js
+++ src/Themes/Fonts.js
@@ -1,20 +1,22 @@
 const fonts = {
   /*
   Real object
-
-  primaryBaseFont: "SF-Pro-Display-Regular",
-  primaryLightFont: "SF-Pro-Display-Light",
-  primaryBoldFont: "SF-Pro-Display-Medium",
+  */
+  /*
+  primaryBaseFont: "sf-pro-display-regular-webfont",
+  primaryLightFont: "system font",
+  primaryBoldFont: "sf-pro-display-medium-webfont"
+  */
   titleBoldFont: "Fontin-Regular",
   BaseFont: "SF-Pro-Display-Regular",
   LightFont: "SF-Pro-Display-Light",
-  BoldFont: "SF-Pro-Display-Medium"
+  BoldFont: "SF-Pro-Display-Bold"
 
-  */
-  primaryBaseFont: "system font",
+  /*
+  primaryBaseFont: "AmaticSC-Regular",
   primaryLightFont: "system font",
-  primaryBoldFont: "system font",
-  titleBoldFont: "Fontin-Regular"
+  primaryBoldFont: "Amatic-Bold",
+  titleBoldFont: "Fontin-Regular"*/
 };
 
 export default fonts;
