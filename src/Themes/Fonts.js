3557a5bf277b8fa44d2910dfc83f4694fddb2379
Marco Franceschi <marcofranceschi@hotmail.com>
2018-03-27 17:16:52 -0400

    Set bold title font

--- src/Themes/Fonts.js
+++ src/Themes/Fonts.js
@@ -2,21 +2,10 @@ const fonts = {
   /*
   Real object
   */
-  /*
-  primaryBaseFont: "sf-pro-display-regular-webfont",
-  primaryLightFont: "system font",
-  primaryBoldFont: "sf-pro-display-medium-webfont"
-  */
   titleBoldFont: "Fontin-Regular",
   BaseFont: "SF-Pro-Display-Regular",
   LightFont: "SF-Pro-Display-Light",
   BoldFont: "SF-Pro-Display-Bold"
-
-  /*
-  primaryBaseFont: "AmaticSC-Regular",
-  primaryLightFont: "system font",
-  primaryBoldFont: "Amatic-Bold",
-  titleBoldFont: "Fontin-Regular"*/
 };
 
 export default fonts;
