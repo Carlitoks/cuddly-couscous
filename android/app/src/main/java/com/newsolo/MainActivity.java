be916cc98a83298ee491f0e197d3f55a3d75e868
Marco Franceschi <marcofranceschi@hotmail.com>
2018-04-06 14:18:03 -0400

    Fixed push notification and opentok config on android

--- android/app/src/main/java/com/newsolo/MainActivity.java
+++ android/app/src/main/java/com/newsolo/MainActivity.java
@@ -2,7 +2,6 @@ package com.newsolo;
 
 import android.os.Bundle; // here
 
-import org.devio.rn.splashscreen.SplashScreen; // here
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
@@ -19,7 +18,6 @@ public class MainActivity extends ReactActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen.show(this);  // here
         super.onCreate(savedInstanceState);
     }
 }
