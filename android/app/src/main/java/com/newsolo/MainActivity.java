e3e6f770b63e79d3aa8efd7ed012f8bc812453d6
Marco Franceschi <marcofranceschi@hotmail.com>
2018-04-06 14:07:30 -0400

    Set configuration of fcm on android

--- android/app/src/main/java/com/newsolo/MainActivity.java
+++ android/app/src/main/java/com/newsolo/MainActivity.java
@@ -1,5 +1,8 @@
 package com.newsolo;
 
+import android.os.Bundle; // here
+
+import org.devio.rn.splashscreen.SplashScreen; // here
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
@@ -11,5 +14,12 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "NewSolo";
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  // here
+        super.onCreate(savedInstanceState);
     }
 }
