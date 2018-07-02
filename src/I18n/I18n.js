d326d89492459d2a9968e25cb12a3f7f83dbc105
Evan Villemez <evillemez@gmail.com>
2018-07-02 16:00:09 -0400

    importing new translations

--- src/I18n/I18n.js
+++ src/I18n/I18n.js
@@ -1,10 +1,16 @@
 import I18n from "react-native-i18n";
 import en from "./Locales/en";
+import ja from "./Locales/ja";
+import zh_hans from "./Locales/zh-hans";
+import zh_hant from "./Locales/zh-hant";
 
 I18n.fallbacks = true;
 
 I18n.translations = {
-  en
+  en,
+  ja,
+  'zh-hans': zh_hans,
+  'zh-hant': zh_hant
 };
 
 export const strings = (name, params = {}) => I18n.t(name, params);
