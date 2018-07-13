5d66b1ee0f52fe3d2a69d2a559e2836c86b7b4e6
Evan Villemez <evan@globalprofessionalsearch.com>
2018-07-13 09:18:56 -0400

    removing placeholder translations

--- src/I18n/I18n.js
+++ src/I18n/I18n.js
@@ -1,19 +1,10 @@
 import I18n from "react-native-i18n";
 import en from "./Locales/en";
-import es from "./Locales/es";
-import zh from "./Locales/zh";
-import zhHans from "./Locales/zh-hans";
-import zhHant from "./Locales/zh-hant";
 
 I18n.fallbacks = true;
 
 I18n.translations = {
-  es,
-  en,
-  "en-US": en,
-  zh,
-  "zh-hans": zhHans,
-  "zh-hant": zhHant
+  en
 };
 
 export const strings = (name, params = {}) => I18n.t(name, params);
