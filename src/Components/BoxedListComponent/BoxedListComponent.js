267dede072d0bd4b7df1391133d94f38190f02c9
Evan Villemez <evan@globalprofessionalsearch.com>
2018-07-18 15:33:22 -0400

    added titleFunc prop to BoxedListComponent

--- src/Components/BoxedListComponent/BoxedListComponent.js
+++ src/Components/BoxedListComponent/BoxedListComponent.js
@@ -29,6 +29,7 @@ const height = Dimensions.get("window").height;
     multiple: Boolean to allow multiple selection on list,
     selected: Index(es) of selected item(s). If there is no selected item(s) value will be -1 or [] in case multiple is true
     titleProperty: if defined, will be the propery used to display each item title,
+    titleFunc: if defined will use a function to return the title, given the item
     other: Object to add to the list with different behaviour (the object must have a boolean property named "other" and be set to true,
     otherOnPress: Function to execute when the other item is selected,
     changeSelected: Function to change on parent the selected value,
@@ -134,8 +135,12 @@ class BoxedListComponent extends Component {
   }
 
   getTitle = item => {
-    const { titleProperty } = this.props;
+    const { titleProperty, titleFunc } = this.props;
     const titleParam = titleProperty ? titleProperty : "id";
+
+    if (titleFunc) {
+      return titleFunc(item);
+    }
 
     return item[titleParam];
   };
