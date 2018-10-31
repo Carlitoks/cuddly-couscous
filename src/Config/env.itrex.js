83670619ff244bd1f1e8922e410a50e2c633496a
Evan Villemez <evillemez@gmail.com>
2018-10-31 10:39:28 -0400

    added itrex environment

---
+++ src/Config/env.itrex.js
@@ -0,0 +1,4 @@
+export const URL = "http://18.191.116.237:5110/v1/"; // ITRex dev server
+export const TOKBOX_APIKEY = "46202112";
+export const stripePublishableKey = "pk_test_VJ3Qq3KbSrkFGIjughMh88Hv";
+export const instabugToken = "83f07c5f8dcb8496e3287f280ce6f61d";
