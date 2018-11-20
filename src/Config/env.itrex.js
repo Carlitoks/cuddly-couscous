e03d080f14219875b7d65c98250098929f0135f1
Evan Villemez <evillemez@gmail.com>
2018-11-19 22:54:52 -0500

    updated itrex environment for codepush

--- src/Config/env.itrex.js
+++ src/Config/env.itrex.js
@@ -2,5 +2,5 @@ export const URL = "http://18.191.116.237:5110/v1/"; // ITRex dev server
 export const TOKBOX_APIKEY = "46202112";
 export const stripePublishableKey = "pk_test_VJ3Qq3KbSrkFGIjughMh88Hv";
 export const instabugToken = "83f07c5f8dcb8496e3287f280ce6f61d";
-export const codePushiOSKey = "8dUSnL_VRozMNV3bqiZE4FbvUdnxHJr2wBnk7";
-export const codePushAndroidKey = "WRbh3xWkrhDWABmePyWNSisdGTvyrkW_H_SnyX";
+export const codePushiOSKey = "5GPetYkgqkaZiSXSAzjbQiRTHsu1H1lJEb-Rm";
+export const codePushAndroidKey = "Ltk63ltZ1T_1317CwYx0MpxKJLLJHk_MVZZC7";
