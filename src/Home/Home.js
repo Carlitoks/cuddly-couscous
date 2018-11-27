c155a36d9bbe94841b7b9b3fb4f836131a2bda94
Esbruiz <ebruiz@teravisiontech.com>
2018-11-27 13:36:56 -0500

    Checking token on appStart and using the refresh token Listener. (#1533)

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -6,6 +6,9 @@ import HomeCustomer from "./Customer/HomeCustomer";
 import HomeLinguist from "./Linguist/HomeLinguist";
 import LoginView from "../Onboarding/LoginView/LoginView";
 import PushNotifications from "../Util/PushNotification";
+import FCM, { FCMEvent } from "react-native-fcm";
+import { registerFCM } from '../Ducks/PushNotificationReducer';
+import { User } from "../Api";
 
 class Home extends Component {
   componentWillMount() {
@@ -15,6 +18,37 @@ class Home extends Component {
     if (!isLoggedIn) {
       navigation.dispatch({ type: "LoginView" });
     }
+
+    this.updateTokenListener = FCM.on(FCMEvent.RefreshToken, FCMtoken => {
+      User.updateDevice(uuid, this.props.deviceId, token, {
+        notificationToken: FCMtoken
+      })
+        .then(res => {
+          this.props.registerFCM({ tokenFCM: FCMtoken });
+        })
+        .catch(error =>  console.log(error));
+    });
+  }
+
+  updateFCMToken = () => {
+    FCM.getFCMToken().then(token => {
+      // save token in sever
+      User.updateDevice(this.props.uuid, this.props.deviceId, this.props.token, { notificationToken: token })
+        .then(res => {
+          this.props.registerFCM({ tokenFCM: token });
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    });
+  };
+
+  componentDidMount() {
+    this.updateFCMToken();
+  }
+
+  componentWillUnmount(){
+    this.updateTokenListener.remove();
   }
 
   render() {
@@ -34,10 +68,13 @@ const mS = state => ({
   isLoggedIn: state.auth.isLoggedIn,
   token: state.auth.token,
   uuid: state.auth.uuid,
+  deviceId: state.auth.deviceId,
   linguistProfile: state.userProfile.linguistProfile,
   nativeLangCode: state.userProfile.nativeLangCode
 });
 
-const mD = {};
+const mD = {
+  registerFCM,
+};
 
 export default connect(mS, mD)(Home);
