94217e3d947da4a0c135cadecbbbabc0eb837bb8
Esteban Ruiz <ebruiz@teravisiontech.com>
2019-01-08 22:18:00 -0500

    Fix for scrolling text disappearing

--- src/Containers/CustomerHome/Components/Partials/Questions.js
+++ src/Containers/CustomerHome/Components/Partials/Questions.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Text, View } from "react-native";
 import Carousel from "react-native-snap-carousel";
 import I18n from "./../../../../I18n/I18n";
-import { ScrollVertical } from "react-native-carousel-text";
+import ScrollVertical from "../../../../Util/ScrollText";
 
 // Styles
 import styles from "./Styles/QuestionStyles";
@@ -30,8 +30,8 @@ export default class Questions extends Component {
 
   _renderItem = (item, index) => {
     return (
-      <View style={styles.questionsContainer}>
-        <Text style={styles.questionText} key={index}>{`${I18n.t(
+      <View key={index} style={styles.questionsContainer}>
+        <Text style={styles.questionText}>{`${I18n.t(
           `customerHome.scenarios.${item}`
         )}`}</Text>
       </View>
@@ -51,19 +51,18 @@ export default class Questions extends Component {
           onChange={index => {
             this.index = index;
           }}
+          onLoad={() => {
+            this.forceUpdate();
+          }}
           enableAnimation
           enableTouchable={false}
           delay={8000}
           duration={0}
           scrollHeight={50}
         >
-          {this.state.scenarios.length > 0 ? (
-            this.state.scenarios.map((scenario, index) => {
-              return this._renderItem(scenario, index);
-            })
-          ) : (
-            <React.Fragment />
-          )}
+          {this.state.scenarios.map((scenario, index) => {
+            return this._renderItem(scenario, index);
+          })}
         </ScrollVertical>
       </React.Fragment>
     );
---
+++ src/Util/ScrollText.js
@@ -0,0 +1,143 @@
+import React, { Component } from 'react';
+import { Animated, Easing, StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'transparent',
+    overflow: 'hidden',
+  },
+  textDefault: {
+    fontSize: 16,
+    color: '#181818',
+  },
+});
+
+class ScrollVertical extends Component {
+
+  constructor(props) {
+    super(props);
+    const translateValue = new Animated.ValueXY({ x: 0, y: 0 });
+    translateValue.addListener(({ x, y }) => {
+    });
+    this.state = {
+      translateValue,
+      scrollHeight: this.props.scrollHeight || 32,
+      content: [],
+      tempValue: 0,
+      contentOffsetY: 0,
+      delay: this.props.delay || 1500,
+      duration: this.props.duration || 500,
+      enableAnimation: true,
+      enableTouchable: true,
+    };
+  }
+
+  componentDidMount() {
+    this.props.onLoad();
+    this.startAnimation();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const children = this.props.children || [];
+    const pages = [];
+    if (children && children.length > 1) {
+      for (let i = 0; i < children.length; i += 1) {
+        pages.push(children[i]);
+      }
+    } else if (children.length === 1) {
+      pages.push(children[0]);
+    } else if (children.length === 0) {
+    } else if (children) {
+      pages.push(children);
+    } else {
+      return (
+        <Text style={{ backgroundColor: 'white' }}>
+      You are supposed to add children inside Carousel
+      </Text>
+    );
+    }
+    if (pages.length !== 0) {
+      const h = (pages.length + 1) * this.state.scrollHeight;
+      this.setState({
+        content: pages,
+        contentOffsetY: h,
+      });
+    }
+    this.setState({ enableAnimation: !!nextProps.enableAnimation }, () => {
+      this.startAnimation();
+  });
+    return null;
+  }
+
+  componentWillUnmount() {
+    if (this.animation) {
+      clearTimeout(this.animation);
+    }
+    if (this.state.translateValue) {
+      this.state.translateValue.removeAllListeners();
+    }
+  }
+
+  startAnimation() {
+    if (this.state.enableAnimation) {
+      if (!this.animation) {
+        this.animation = setTimeout(() => {
+          this.animation = null;
+        this.beginAnim();
+      }, this.state.delay);
+      }
+    }
+  }
+
+  beginAnim() {
+    this.state.tempValue -= this.state.scrollHeight;
+    if (this.props.onChange) {
+      const index = Math.abs(this.state.tempValue) / (this.state.scrollHeight);
+      this.props.onChange(index < this.state.content.length - 1 ? index : 0);
+    }
+    Animated.sequence([
+      Animated.timing(
+        this.state.translateValue,
+        {
+          isInteraction: false,
+          easing: Easing.linear,
+          duration: this.state.duration,
+          toValue: { x: 0, y: this.state.tempValue },
+        },
+      ),
+    ]).start(() => {
+      if (this.state.tempValue - this.state.scrollHeight === -this.state.contentOffsetY) {
+      this.state.translateValue.setValue({ x: 0, y: 0 });
+      this.state.tempValue = 0;
+    }
+    this.startAnimation();
+  });
+  }
+
+  render() {
+    return (
+      <View
+    style={[styles.container, { height: this.state.scrollHeight }, this.props.container]}
+  >
+    {
+      this.state.content.length > 1
+        ? <Animated.View
+      style={[
+          {
+            flexDirection: 'column',
+            transform: [{ translateY: this.state.translateValue.y }],
+          }]}
+        >
+        {this.state.content}
+    </Animated.View>
+    : this.state.content
+    }
+  </View>
+  );
+  }
+}
+
+ScrollVertical.defaultProps = {
+  enableAnimation: true,
+};
+export default ScrollVertical;
