a772ba64aa4432d0cbe9c7db6928ad313d0aa4d3
Evan Villemez <evillemez@gmail.com>
2019-03-28 18:40:21 -0400

    call control styling

--- src/Containers/Session/Components/SessionControls.js
+++ src/Containers/Session/Components/SessionControls.js
@@ -1,57 +1,95 @@
 import React from "react";
-import {View, Button, StyleSheet} from "react-native";
+import {View, StyleSheet} from "react-native";
 
-import TextButton from "../../../Components/Widgets/TextButton";
+import Icon from "react-native-vector-icons/Ionicons";
+import colors from "../../../Themes/Colors";
+
+const IconButton = ({name, enabled, onPress}) => {
+  return (
+    <Icon.Button
+      name = {name}
+      borderRadius={100}
+      containerViewStyle={{
+        borderRadius: 100,
+        opacity: enabled ? 0.7 : 0.27,
+        textAlign: "center",
+      }}
+      backgroundColor={ enabled ? colors.primaryColor : "rgba(255,255,255,0.27)" }
+      onPress ={() => { onPress() }}
+      style={{
+        height: 47,
+        width: 47,
+        justifyContent: "center",
+        borderRadius: 100
+      }}
+      iconStyle={{
+        marginRight: 0,
+      }}
+      size={32}
+      color={enabled ? colors.gradientColor.top : colors.primaryColor}
+    />
+  );
+}
 
 export const SessionControls = (props) => {
   return (
     <View style={styles.container}>
-      <TextButton
-        text = "Camera"
-        onPress = {()=> { props.onCameraFlipPressed() }}
-        style = { buttonStyle(props.cameraFlipEnabled) }
-        textStyle = { styles.buttonText }
+
+      {/* camera flip button */}
+      <IconButton
+        name={"ios-reverse-camera-outline"}
+        enabled={props.cameraFlipEnabled}
+        onPress={props.onCameraFlipPressed}
       />
-      <TextButton
-        text = "Speaker"
-        onPress = {()=> { props.onSpeakerPressed() }}
-        style = { buttonStyle(props.speakerEnabled) }
-        textStyle = { styles.buttonText }
+
+      {/* speaker button */}
+      <IconButton
+        name={"ios-volume-up"}
+        enabled={props.speakerEnabled}
+        onPress={props.onSpeakerPressed}
       />
-      <TextButton
-        text = "End"
-        style = {{...b, backgroundColor: "#800"}}
-        onPress = {() => { props.onEndCallPressed() }}
-        textStyle = { styles.buttonText }
+
+      {/* end call button */}
+      <Icon.Button
+        name = {'md-call'}
+        borderRadius={100}
+        containerViewStyle={{
+          borderRadius: 100,
+          textAlign: "center",
+        }}
+        backgroundColor={ 'red' }
+        onPress ={() => { props.onEndCallPressed() }}
+        style={{
+          height: 60,
+          width: 60,
+          justifyContent: "center",
+          borderRadius: 100
+        }}
+        iconStyle={{
+          marginRight: 0,
+          transform: [{rotate: '135deg'}]
+        }}
+        size={40}
+        color={'white'}
       />
-      <TextButton
-        text = "Mic"
-        onPress = {()=> { props.onMicPressed() }}
-        style = { buttonStyle(props.micEnabled) }
-        textStyle = { styles.buttonText }
+
+      {/* mute button */}
+      <IconButton
+        name={"ios-mic-off"}
+        enabled={!props.micEnabled}
+        onPress={props.onMicPressed}
       />
-      <TextButton
-        text = "Video"
-        onPress = {()=> { props.onVideoPressed() }}
-        style = { buttonStyle(props.videoEnabled) }
-        textStyle = { styles.buttonText }
-      />    
+
+      {/* video button */}
+      <IconButton
+        name={"ios-videocam-outline"}
+        enabled={props.videoEnabled}
+        onPress={props.onVideoPressed}
+      />
+      
     </View>
   );
 };
-
-const b = {
-  flex: 1,
-  height: 40,
-  margin: 3
-}
-
-const buttonStyle = (enabled) => {
-  return {
-    ...b,
-    backgroundColor: enabled ? "#444488" : "#884444",
-  };
-}
 
 const styles = StyleSheet.create({
   container: {
@@ -59,9 +97,11 @@ const styles = StyleSheet.create({
     width: "100%",
     backgroundColor: 'rgba(0, 0, 0, 0.33)',
     paddingTop: 5,
-    paddingBottom: 5
+    paddingBottom: 5,
+    paddingLeft: 15,
+    paddingRight: 15,
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    // paddingBottom: TODO: add extra padding for new iPhones
   },
-  buttonText: {
-    color: "#fff"
-  }
 });
