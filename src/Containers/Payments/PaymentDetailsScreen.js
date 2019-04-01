6305a2541641aaaf7df5df75d1c8730a8b0a6d13
Pedro Garcia <pgarcia@teravisiontech.com>
2019-04-01 11:40:03 -0400

    paymentScreenDetail file

---
+++ src/Containers/Payments/PaymentDetailsScreen.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import { ScrollView, View, Alert, Image, Text } from "react-native";
+import { connect } from "react-redux";
+import Header from "../CustomerHome/Components/Header";
+import ViewWrapper from "../ViewWrapper/ViewWrapper";
+import PaymentButtons from "./Components/PaymentButtons";
+import NoCardImage from "./Components/NoCardImage";
+import AddCard from "./Components/AddCard";
+
+// Styles
+import styles from "./Styles/PaymentScreenStyles";
+import metrics from "../../Themes/Metrics";
+import stripe from "tipsi-stripe";
+import { stripePublishableKey } from "../../Config/env";
+
+const addCard = require("../../Assets/Images/addcard.png");
+
+class PaymentScreen extends Component {
+  componentWillMount() {
+    stripe.setOptions({
+      publishableKey: stripePublishableKey,
+      androidPayMode: "test" // Android only
+    });
+  }
+
+  render() {
+    const { navigation } = this.props;
+    return (
+      <ViewWrapper style={styles.wrapperContainer}>
+        <View style={[styles.mainContainer]}>
+          <Header navigation={navigation} />
+          <ScrollView
+            automaticallyAdjustContentInsets
+            alwaysBounceVertical={false}
+            contentContainerStyle={styles.scrollViewFlex}
+          >
+            <PaymentButtons navigation={navigation} />
+          </ScrollView>
+        </View>
+      </ViewWrapper>
+    );
+  }
+}
+
+const mS = state => ({
+  token: state.auth.token,
+  uuid: state.auth.uuid
+});
+
+const mD = {};
+
+export default connect(
+  mS,
+  mD
+)(PaymentScreen);
