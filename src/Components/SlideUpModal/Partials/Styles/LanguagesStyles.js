70d9595c716222ed3fbc1944b724b38f47eb84c4
Pedro Garcia <pgarcia@teravisiontech.com>
2019-05-03 17:22:25 -0400

    #1968_fixes: Auto stash before merge of "issue_#1968_fixes" and "origin/issue_#1968_fixes"

--- src/Components/SlideUpModal/Partials/Styles/LanguagesStyles.js
+++ src/Components/SlideUpModal/Partials/Styles/LanguagesStyles.js
@@ -1,22 +1,22 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 import { moderateScale, moderateScaleViewports } from "../../../../Util/Scaling";
-import { Fonts, Metrics } from '../../../../Themes';
+import { Fonts, Metrics } from "../../../../Themes";
 
 export default StyleSheet.create({
   scrollContainer: {
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     left: Metrics.width * 0.05,
-    width: Metrics.width * 0.90,
+    width: Metrics.width * 0.9
   },
   availableLangContainer: {
     height: 48,
-    justifyContent: 'center',
-    backgroundColor: '#EBEBEB',
+    justifyContent: "center",
+    backgroundColor: "#EBEBEB",
     borderRadius: 10,
     left: Metrics.width * 0.05,
-    width: Metrics.width * 0.90,
+    width: Metrics.width * 0.9,
     borderBottomLeftRadius: 0,
-    borderBottomRightRadius: 0,
+    borderBottomRightRadius: 0
   },
   availableLangContainerText: {
     paddingLeft: 19,
@@ -24,29 +24,35 @@ export default StyleSheet.create({
     fontSize: moderateScaleViewports(15, 0),
     color: '#1C1B1B'
   },
-  dividerStyle: { backgroundColor: "rgba(90, 90, 90, 0.2)", height: 0.5, width: Metrics.width * 0.80, marginLeft: Metrics.width * 0.05 },
+  dividerStyle: {
+    backgroundColor: "rgba(90, 90, 90, 0.2)",
+    height: 0.5,
+    width: Metrics.width * 0.8,
+    marginLeft: Metrics.width * 0.05
+  },
   unAvailableLangContainer: {
     height: 48,
-    width: '100%',
-    backgroundColor: '#EBEBEB',
-    justifyContent: 'center',
+    width: "100%",
+    backgroundColor: "#EBEBEB",
+    justifyContent: "center"
   },
   unAvailableLangContainerText: {
     paddingLeft: 19,
+    borderWidth: 0,
     fontFamily: Fonts.BaseFont,
     fontSize: moderateScaleViewports(15, 0),
     color: '#1C1B1B'
   },
   LangViewContainer: {
     height: 48,
-    width: '100%',
-    justifyContent: 'center',
-    backgroundColor: '#F4F4F4'
+    width: "100%",
+    justifyContent: "center",
+    backgroundColor: "#F4F4F4"
   },
   selectLangButton: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignContent: 'center'
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignContent: "center"
   },
   unAvailableLangText: {
     paddingLeft: 45,
@@ -60,5 +66,5 @@ export default StyleSheet.create({
     fontSize: moderateScaleViewports(16),
     color: "#1C1B1B"
   },
-  checkPadding: { paddingRight: 30 },
+  checkPadding: { paddingRight: 30 }
 });
