4dc012270414513b990569a9c476761a9a0b09be
Evan Villemez <evillemez@gmail.com>
2019-05-09 22:48:46 -0400

    minor style refactor

--- src/Containers/Session/SessionView.js
+++ src/Containers/Session/SessionView.js
@@ -1016,11 +1016,7 @@ const styles = StyleSheet.create({
     backgroundColor: colors.backgroundBlue
   },
   controlContainer: {
-    position: 'absolute',
-    top: 0,
-    left:0,
-    bottom: 0,
-    right: 0,
+    ...StyleSheet.absoluteFill
   },
   controls: {
     flex: 1,
