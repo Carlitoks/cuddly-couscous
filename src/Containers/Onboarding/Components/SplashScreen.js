a32dd823138c1c02b7df9e5e7e58d2132e15972a
Esteban <esbruiz@outlook.es>
2019-05-16 14:15:00 -0500

    Change logo and fade in.

--- src/Containers/Onboarding/Components/SplashScreen.js
+++ src/Containers/Onboarding/Components/SplashScreen.js
@@ -3,19 +3,20 @@ import { View, Image, TextInput, Text, Platform, Animated } from "react-native";
 
 //styles
 import styles from "./Styles/SplashScreenStyles";
+import metrics from "../../../Themes/Metrics";
 
 const logo = require("../../../Assets/Images/JeenieLoadingLogo.png");
 
 class SplashScreen extends React.Component {
   state = {
-    fadeAnim: new Animated.Value(1),  // Initial value for opacity: 0
+    fadeAnim: new Animated.Value(0),  // Initial value for opacity: 0
   };
 
   componentDidMount() {
     Animated.timing(                  // Animate over time
       this.state.fadeAnim,            // The animated value to drive
       {
-        toValue: 0,                   // Animate to opacity: 1 (opaque)
+        toValue: 1,                   // Animate to opacity: 1 (opaque)
         duration: 1500,              // Make it take a while
       }
     ).start();                        // Starts the animation
@@ -25,7 +26,7 @@ class SplashScreen extends React.Component {
     let { fadeAnim } = this.state;
     return (
       <View style={styles.splashScreenContainer}>
-        <Animated.Image style={{opacity: fadeAnim}} source={logo} />
+        <Animated.Image resizeMethod={"resize"} style={{opacity: fadeAnim, width: metrics.width * 0.70, height: metrics.height * 0.50, resizeMode: "contain"}} source={logo} />
       </View>);
   }
 };
