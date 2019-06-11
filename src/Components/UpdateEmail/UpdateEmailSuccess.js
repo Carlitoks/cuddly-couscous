5c3fdde86e62840a9fd2c6e0e78baefdc5a0eb2a
Evan Villemez <evillemez@gmail.com>
2019-06-11 11:41:27 -0400

    removed userProfile refs from UpdateEmailSuccess

--- src/Components/UpdateEmail/UpdateEmailSuccess.js
+++ src/Components/UpdateEmail/UpdateEmailSuccess.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { Modal, Text, TouchableOpacity, View } from "react-native";
 
 import styles from "./styles";
-import { updateView as closeUpdateEmail } from "../../Ducks/UserProfileReducer";
 import { logOut } from "../../Ducks/AuthReducer2";
 import { connect } from "react-redux";
 import I18n from "../../I18n/I18n";
@@ -10,10 +9,9 @@ import {update as updateLogicReducer} from "../../Ducks/LogicReducer";
 
 class UpdateEmailSuccess extends Component {
   render() {
-    const { navigation, logOut, updateLogicReducer, closeUpdateEmail, email } = this.props;
+    const { navigation, logOut, updateLogicReducer, user } = this.props;
     const handleClose = async () => {
       await updateLogicReducer({emailEditSuccess: false});
-      await closeUpdateEmail({ emailBounced: false });
       await logOut().finally(() => {navigation.dispatch({type: "LoginView"})});
     };
     return (
@@ -21,7 +19,7 @@ class UpdateEmailSuccess extends Component {
             <View style={styles.innerContainer}>
               <View style={styles.topContainer}>
                 <Text style={styles.titleText}>{I18n.t("correctEmailModal.titleSuccess")}</Text>
-                <Text style={styles.subtitleText}>{I18n.t("correctEmailModal.descriptionSuccess", {email})}</Text>
+                <Text style={styles.subtitleText}>{I18n.t("correctEmailModal.descriptionSuccess", {email: user.email})}</Text>
               </View>
               <View style={styles.buttonContainer}>
                 <TouchableOpacity style={styles.closeModal} onPress={() => handleClose()}>
@@ -35,13 +33,11 @@ class UpdateEmailSuccess extends Component {
 };
 
 const mS = state => ({
-  emailBounced: state.userProfile.emailBounced,
-  email: state.userProfile.email,
+  user: state.account.user,
 });
 
 const mD = {
   logOut,
-  closeUpdateEmail,
   updateLogicReducer
 };
 
