c81eeb4d2264772317ca6b35f9712a46e2e7715b
carlitoks <cjrmarin@gmail.com>
2019-06-17 17:39:08 -0400

    issue #2228

--- src/Containers/Register/Components/EmailField.js
+++ src/Containers/Register/Components/EmailField.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Text, TextInput, View } from "react-native";
 import { connect } from "react-redux";
+import debounce from "lodash/debounce";
 // Styles
 import styles from "./Styles/EmailFieldStyles";
 import I18n from "../../../I18n/I18n";
@@ -9,12 +10,18 @@ import { update as updateOnboarding } from "../../../Ducks/OnboardingReducer";
 
 
 class EmailField extends Component {
-  isValidEmail = text => {
+  isValidEmail = (text) => {
     const { updateOnboarding, errorType } = this.props;
     const reg = new RegExp(EMAIL_REGEX);
+
+    // debounce to not show error message while user is typing
+    const debounced = debounce((e) => {
+      updateOnboarding(e);
+    }, 2000);
+
     if (text) {
       if (!reg.test(text) && text.length > 4) {
-        updateOnboarding({
+        debounced({
           isValidEmail: false,
           errorType: "emailFormat",
         });
@@ -29,13 +36,13 @@ class EmailField extends Component {
   };
 
   render() {
-    const { email, errorType, setRef, nextInput,isValidEmail } = this.props;
+    const { email, errorType, setRef, nextInput, isValidEmail } = this.props;
     return (
       <View style={email ? styles.inputViewContainerValue : styles.inputViewContainer}>
         {email ? <Text style={styles.labelText}>{I18n.t("email")}</Text> : <Text style={styles.labelText} />}
         <View style={styles.inputsErrorContainer}>
           <TextInput
-            ref={input => {
+            ref={(input) => {
               setRef(input);
             }}
             allowFontScaling={false}
@@ -52,7 +59,7 @@ class EmailField extends Component {
             returnKeyType="done"
           />
         </View>
-        {(!isValidEmail || errorType === "AlreadyRegistered" ) && (email && email.length > 6) ? (
+        {(!isValidEmail || errorType === "AlreadyRegistered") && (email && email.length > 6) ? (
           <Text style={styles.invalidLabelText}>{I18n.t("noValidEmail")}</Text>
         ) : (
           <Text style={styles.invalidLabelText} />
