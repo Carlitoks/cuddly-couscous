964a5d88f7380b6692f8f74e9c469d3b971efaf9
Andres <arocha.p43@gmail.com>
2019-07-03 18:42:08 -0400

    PromoCode fixed

--- src/Containers/Payments/Components/Styles/PromoCodeStyles.js
+++ src/Containers/Payments/Components/Styles/PromoCodeStyles.js
@@ -7,27 +7,32 @@ export default StyleSheet.create({
   ...ApplicationStyles.screen,
   promoCodeContainer: {
     width: metrics.width * 0.9,
+    backgroundColor:"#FFFFFF",
+    flexDirection:"column",
+    height:"15%",
+    paddingTop: 10,
+    paddingLeft:20,
+    paddingBottom: 20,
   },
   promoCodeLabel: {
     fontFamily: Fonts.BaseFont,
     fontSize: 14,
     alignItems: 'center',
     color: 'black',
+    fontWeight:"bold",
+    paddingBottom:5,
   },
-
   apply: {
     width: 73,
     height: 40,
     color: '#401674',
   },
   inputContainer:{
-    backgroundColor: 'red',
-    height: 40,
-    width: "90%",
-    flexDirection: 'row',
-    justifyContent: 'center',
-    position:"relative"
-    
+    backgroundColor: 'blue',
+    height:40,
+    width:"90%",
+    flex:1,
+    paddingTop:10,
   },
   button:{
     flexDirection: 'column',
@@ -46,12 +51,62 @@ export default StyleSheet.create({
     fontWeight: 'bold'
   },
   input:{
+    paddingLeft: moderateScaleViewports(5),
     height: 40, 
     borderColor: 'gray', 
     borderWidth: 1,
     width: '80%',
     backgroundColor: "#F5F5F5",
-    position:"absolute",
     left:0
-  }
+  },
+  infoContainer:{
+    flexDirection: "row",
+    height:20,
+    width:"95%",
+    marginTop: 13,
+    position:"relative"
+    
+  },
+  remove: {
+    fontFamily: Fonts.BaseFont,
+    fontSize: 14,
+    alignItems: 'center',
+    color: '#007AFF',
+    position:"absolute",
+    right:0,
+  },
+  message: {
+    fontFamily: Fonts.BaseFont,
+    fontSize: 14,
+    alignItems: 'center',
+    color: 'rgba(0, 0, 0, 0.9);',
+  },
+  messageError:{
+    fontFamily: Fonts.BaseFont,
+    fontSize: 14,
+    alignItems: 'center',
+    color: '#F53333',
+  },
+  inputError:{
+    paddingLeft: moderateScaleViewports(5),
+    height: 40, 
+    borderColor: '#FF3B30', 
+    borderWidth: 1,
+    width: '80%',
+    color:"#FF3B30",
+    backgroundColor: "#F5F5F5",
+    left:0
+  },
+  button:{
+    flexDirection: 'column',
+    justifyContent: 'center',
+    width: moderateScaleViewports(73),
+    height: moderateScaleViewports(40),
+    backgroundColor: '#FF3B30',
+    borderTopRightRadius: moderateScaleViewports(10),
+    borderBottomRightRadius: moderateScaleViewports(10),
+    position:"absolute",
+    right:0,
+  },
+
 });
