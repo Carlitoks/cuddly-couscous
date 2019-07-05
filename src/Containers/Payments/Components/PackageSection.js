560b3d1f36ed7fc33c6a64ba6a3e2e594a93f745
Unknown <arocha.p43@gmail.com>
2019-07-04 21:28:20 -0400

    Correction partial version

--- src/Containers/Payments/Components/PackageSection.js
+++ src/Containers/Payments/Components/PackageSection.js
@@ -10,9 +10,6 @@ const packageImage = require("../../../Assets/Images/packageImage.png");
 class PackageSection extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      loading:false
-    };
   }
 
   render() {
@@ -20,55 +17,30 @@ class PackageSection extends Component {
 
     return (
       <View style={styles.packageContainer}>
-
-      {!userPackage && <View style={styles.row}>  
-          <Text style={styles.creditCardTitle}>{I18n.t("account.package.title")}</Text>
-        <View style={styles.buttonContainer}>  
-          <TouchableOpacity
-            onPress={() => {this.props.addPackage()}}
-            style={styles.addCardButton }
-          >
-            <Text
-              style={styles.addCardButtonText}
-            >
-              {I18n.t("account.package.add")}
+        {!userPackage && (
+          <View style={styles.row}>
+            <Text style={styles.creditCardTitle}>{I18n.t("account.package.title")}</Text>
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity
+                onPress={() => {
+                  this.props.addPackage();
+                }}
+                style={styles.addCardButton}
+              >
+                <Text style={styles.addCardButtonText}>{I18n.t("account.package.add")}</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        )}
+        {!userPackage && (
+          <View style={styles.rowDescription}>
+            <Image style={styles.backgroundImage} source={packageImage} />
+            <Text style={styles.description}>
+              {I18n.t("account.package.description", { rate: "US$1" })}
             </Text>
-          </TouchableOpacity>
-        </View>
-      </View>}
-      {!userPackage && <View style={styles.rowDescription}>  
-        <Image style={styles.backgroundImage} source={packageImage} />
-        <Text
-          style={styles.description}
-        >
-          {I18n.t("account.package.description", { rate: "US$1" })}
-        </Text>
-      </View>}
-      {userPackage && 
-        <MinutePackageCard
-        minutePackage = {minutePackage}
-        selectable={true} // show the select button
-        onSelect={ () => navigation.dispatch({type: "PackageCheckoutView", params: {minutePackage}}) } // func to call if select button pressed
-        displayReloadNotice={false} // display the reload notice or not
-        reloadNoticeValue={false} // whether or not the checkbox is selected
-        onReloadNoticeSelect={(val) => {}} // func called when reload notice is selected, or unselected, `val` is a boolean
-        promoCodeActive={true}
-        discountedPrice={4000}
-        special={I18n.t("minutePackage.special")}
-        specialColors={["#F39100", "#FCB753"]}
-      />
-      }
-
-          <TextBlockButton
-                text = "packages.checkout.purchase" // the text in the button
-                disabled = {false} // boolean if disabled, prevents taps and show disabled button styles
-                loading = {this.state.loading} // boolean for "loading" state, in the loading state, display an ActivitySpinner instead of the button text
-                style = {styles.buttonContainer} // main container style, component should provide some defaults, like width at 100%
-                disabledStyle = {styles.buttonDisable} // container style object when disabled, component should provide defaults
-                textStyle = {styles.buttonText} // optional text styles, component should provide defaults
-                onPress = {() => navigation.dispatch({type: "Availablepackages"})} // function to call when pressed
-            />
-    </View>
+          </View>
+        )}
+      </View>
     );
   }
 }
@@ -77,8 +49,7 @@ const mS = state => ({
   StripePaymentSourceMeta: state.account.user.StripePaymentSourceMeta
 });
 
-const mD = {
-};
+const mD = {};
 
 export default connect(
   mS,
