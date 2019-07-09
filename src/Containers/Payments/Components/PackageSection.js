3f80c4e077a6a4af5a73c0abfe3d891b6b1de62f
carlos rodriguez <cjrmarin@gmail.com>
2019-07-09 17:22:37 -0400

    purchases

--- src/Containers/Payments/Components/PackageSection.js
+++ src/Containers/Payments/Components/PackageSection.js
@@ -17,27 +17,42 @@ class PackageSection extends Component {
 
     return (
       <View style={styles.packageContainer}>
-        {!userPackage && (
           <View style={styles.row}>
             <Text style={styles.creditCardTitle}>{I18n.t("account.package.title")}</Text>
             <View style={styles.buttonContainer}>
-              <TouchableOpacity
-                onPress={() => {
-                  this.props.addPackage();
-                }}
-                style={styles.addCardButton}
-              >
-                <Text style={styles.addCardButtonText}>{I18n.t("account.package.add")}</Text>
-              </TouchableOpacity>
+              {!userPackage && (
+                <TouchableOpacity
+                  onPress={() => {
+                    this.props.addPackage();
+                  }}
+                  style={styles.addCardButton}
+                >
+                  <Text style={styles.addCardButtonText}>{I18n.t("account.package.add")}</Text>
+                </TouchableOpacity>
+              )}
             </View>
           </View>
-        )}
-        {!userPackage && (
+        {!userPackage ? (
           <View style={styles.rowDescription}>
             <Image style={styles.backgroundImage} source={packageImage} />
             <Text style={styles.description}>
               {I18n.t("account.package.description", { rate: "US$1" })}
             </Text>
+          </View>
+        ) : (
+          <View style={styles.rowDescription}>
+            <MinutePackageCard 
+              minutePackage = {userPackage}
+              selectable={false} // show the select button
+              onSelect={ () => {} } // func to call if select button pressed
+              displayReloadNotice={true} // display the reload notice or not
+              reloadNoticeValue={false} // whether or not the checkbox is selected
+              onReloadNoticeSelect={(val) => { alert (val)}} // func called when reload notice is selected, or unselected, `val` is a boolean
+              promoCodeActive={!!this.props.minutePackagePromoCode}
+              discountedPrice={navigation.state.params.minutePackage.adjustedCost != navigation.state.params.minutePackage.cost ? navigation.state.params.minutePackage.adjustedCost : false}
+              special={navigation.state.params.minutePackage.public ? false : I18n.t("minutePackage.special")}
+              specialColors={["#F39100", "#FCB753"]}
+            />
           </View>
         )}
       </View>
