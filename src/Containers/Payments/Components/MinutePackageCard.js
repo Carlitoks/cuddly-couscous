c6143c5d08cadc6dd2e3505812a32b73f4597069
carlos rodriguez <cjrmarin@gmail.com>
2019-07-10 19:23:38 -0400

    autoReload true + promocode send to api

--- src/Containers/Payments/Components/MinutePackageCard.js
+++ src/Containers/Payments/Components/MinutePackageCard.js
@@ -16,7 +16,7 @@ class MinutePackageCard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      checked: false,
+      checked: props.reloadNoticeValue,
     };
   }
 
