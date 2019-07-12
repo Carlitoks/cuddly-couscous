4bcba06d53e9d63277773b9e7e276ba4dc63fd06
Andres <arocha.p43@gmail.com>
2019-07-11 23:16:02 -0400

    HomeColor change- issue 2368

--- src/Containers/CustomerHome/Components/Partials/HeaderMinutesLeft.js
+++ src/Containers/CustomerHome/Components/Partials/HeaderMinutesLeft.js
@@ -33,9 +33,9 @@ class HeaderMinutesLeft extends Component {
         {(user.stripePaymentToken && user.availableMinutes > 0) || !user.stripePaymentToken ? (
           <View
             style={
-              user.availableMinutes === 0
+                user.availableMinutes < 6
                 ? styles.outOfMinutesContainer
-                : user.availableMinutes > 0 && user.availableMinutes < 6
+                : user.availableMinutes > 5 && user.availableMinutes < 11
                 ? styles.fewMinutesLeftContainer
                 : styles.minutesLeftInfoContainer
             }
