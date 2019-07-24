fa49b3667755b32719420e6c4d64034f3f31eaf9
Evan Villemez <evillemez@gmail.com>
2019-07-24 13:57:38 -0400

    updated segment api keys

--- src/Config/env.prod.js
+++ src/Config/env.prod.js
@@ -6,7 +6,7 @@ export const stripePublishableKey = "pk_live_IjwD7foeSq0ox5Q3y6GQ3jkv";
 export const instabugToken = "1ef778fa18d0379f12f7ebaed42eba02";
 export const codePushiOSKey = "0KJSeDnom8nacxfuo-r5MJ7m8giMBJSnwBnJ7";
 export const codePushAndroidKey = "WFjG18TuRC9phOCXffSkV8o6B5vWHkurdr21Q";
-export const segmentKey = "faop9ifj8TTxIWifE2VjHAsOTcV4smnW";
+export const segmentKey = "tDhw3qpxNwb0re67M8haeiQtz66r55XX";
 export const forensicsEnabled = true;
 
 export const promptUpdate = false;
--- src/Config/env.staging.js
+++ src/Config/env.staging.js
@@ -6,7 +6,7 @@ export const stripePublishableKey = "pk_test_VJ3Qq3KbSrkFGIjughMh88Hv";
 export const instabugToken = "83f07c5f8dcb8496e3287f280ce6f61d";
 export const codePushiOSKey = "8dUSnL_VRozMNV3bqiZE4FbvUdnxHJr2wBnk7";
 export const codePushAndroidKey = "WRbh3xWkrhDWABmePyWNSisdGTvyrkW_H_SnyX";
-export const segmentKey = "5CzcqoWWNLzu6F9XjIUMIV2gQXryOxdk";
+export const segmentKey = "yEyPKFgLAwIKqKb2H47V8xfD7yLQRlTU";
 export const forensicsEnabled = true;
 
 export const promptUpdate = false;
