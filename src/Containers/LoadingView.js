684427c5cb3d9b58d31ba2fe8aefe574ea521fe8
Esteban Ruiz <esbruiz@outlook.es>
2019-08-01 15:26:57 -0500

    Restoring old loading logic

--- src/Containers/LoadingView.js
+++ src/Containers/LoadingView.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Animated, Platform, View } from "react-native";
+import { Image, Platform, View } from "react-native";
 // styles
 import { connect } from "react-redux";
 import Permission from "react-native-permissions";
@@ -9,74 +9,52 @@ import { moderateScaleViewports } from "../Util/Scaling";
 const logo = require("../Assets/Images/JeenieLoadingLogo.png");
 
 class LoadingView extends React.Component {
-  state = {
-    fadeAnim: new Animated.Value(this.props.animation ? 0 : 1), // Initial value for opacity: 0
-  };
-
-  constructor(props) {
-    super(props);
+  componentDidMount() {
     const {
-      auth2, onboardingReducer, account
+      auth2, onboardingReducer, account, navigation,
     } = this.props;
     const { completedLocation, completedNotification } = onboardingReducer;
     if (auth2.isLoggedIn && auth2.userJwtToken) {
       // extra check to ensure we have a user, because if not
       // we should just stay on this screen
       if (!account.user) {
-        return this.navigateWithDelay("IntroView");
+        return navigation.dispatch({ type: "IntroView" });
       }
 
       if (completedLocation) {
         if (completedNotification) {
-          return this.navigateWithDelay("Home");
+          return navigation.dispatch({ type: "Home" });
         }
         if (Platform.OS === "android") {
-          return this.navigateWithDelay("Home");
+          return navigation.dispatch({ type: "Home" });
         }
         Permission.check("notification").then((permission) => {
           if (permission === "undetermined") {
-            return this.navigateWithDelay("Home");
+            return navigation.dispatch({ type: "Home" });
           }
-          return this.navigateWithDelay("Home");
+          return navigation.dispatch({ type: "Home" });
         });
       } else {
         Permission.check("location").then((permission) => {
           if (permission === "undetermined") {
-            return this.navigateWithDelay("LocationPermissionView");
+            return navigation.dispatch({ type: "LocationPermissionView" });
           }
-          return this.navigateWithDelay("Home");
+          return navigation.dispatch({ type: "Home" });
         });
       }
     } else {
-      return this.navigateWithDelay("IntroView");
+      return navigation.dispatch({ type: "IntroView" });
     }
+    return null;
   }
-
-  componentDidMount() {
-    Animated.timing( // Animate over time
-      this.state.fadeAnim, // The animated value to drive
-      {
-        toValue: 1, // Animate to opacity: 1 (opaque)
-        duration: 1500, // Make it take a while
-      },
-    ).start(); // Starts the animation
-  }
-
-  navigateWithDelay = (routeName) => {
-    const { navigation } = this.props;
-    setTimeout(() => {
-      navigation.dispatch({ type: routeName });
-    }, 1500);
-  };
 
   render() {
-    const { fadeAnim } = this.state;
     return (
       <View style={styles.splashScreenContainer}>
-        <Animated.Image
+        <Image
           resizeMethod="resize"
           style={{
-            opacity: fadeAnim, width: moderateScaleViewports(270), height: moderateScaleViewports(207), resizeMode: "contain",
+            width: moderateScaleViewports(270), height: moderateScaleViewports(207), resizeMode: "contain",
           }}
           source={logo}
         />
