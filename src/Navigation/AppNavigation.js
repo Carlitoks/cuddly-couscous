d2f8587f2c0577b4400f1c655ef83106bfcf93a0
Andres <arocha.p43@gmail.com>
2019-09-11 10:30:43 -0400

    Error initialView

--- src/Navigation/AppNavigation.js
+++ src/Navigation/AppNavigation.js
@@ -41,7 +41,7 @@ import LinguistIncomingCallView from "../Containers/Session/LinguistIncomingCall
 
 // by default, this is the screen that will
 // be shown first when the app loads
-let initialScreen = "MissingRequiredPermissionsView";
+let initialScreen = "LoadingView";
 
 // change the initial screen, this must be called before
 // the router is initialized
